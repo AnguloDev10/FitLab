88405cfd0cddde9634f77accf3461da2a4f382d9
William Angulo <wmavalle@gmail.com>
2022-05-26 16:17:08 -0500

    Adding complaint class

---
+++ Fitlab.Entities/Complaint.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fitlab.Entities
+{
+    public class Complaint
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]  
+        public string Subject { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+    }
+}
