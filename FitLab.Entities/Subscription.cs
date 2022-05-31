0705b0c64f465af50a9ce631131145c04713f0f6
unknown <marcuslk999@gmail.com>
2022-05-31 00:46:55 -0500

    subscriptionv2

--- FitLab.Entities/Subscription.cs
+++ FitLab.Entities/Subscription.cs
@@ -5,12 +5,18 @@ namespace Fitlab.Entities
    
     public class Subscription
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public bool Active { get; set; }
+        [Required]
         public int MaxSessions { get; set; }
+        [Required]
         public int Price { get; set; }
+        [Required]
         public int UserId { get; set; }
         /*
+         
         public User User { get; set; }
         */
 
